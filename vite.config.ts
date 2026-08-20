f884d72187ce9164bbeb728bfa193e22b29a23f3
Wilf Grainger <wjgrainger@gmail.com>
2026-08-20 12:39:23 +0100

    fix: use misfits google oauth client

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const configuredClientId = '283865619073-pbgbcaoamje9rutnq7sq59fd9lmqrme3.apps.googleusercontent.com';
+const configuredClientId = '124399248491-l5t5oro70rfvbc360cpl26v59pgg6cf5.apps.googleusercontent.com';
 
 export default defineConfig({
   plugins: [react()],
