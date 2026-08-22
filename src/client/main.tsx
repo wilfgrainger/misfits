f8277e702943ebfac031cc882e9561e6bd2aae42
wilfgrainger <51911955+wilfgrainger@users.noreply.github.com>
2026-08-22 21:27:55 +0100

    feat: load member experience styles

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './mobile-experience.css';
+import './member-experience.css';
 
 console.log('Misfits 501 Client Initialized');
 
